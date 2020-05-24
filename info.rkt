c806433ce05651f0240c26a38969f88841226866
dannypsnl <dannypsnl@gmail.com>
2020-05-24 21:21:34 +0800

    reporter: init

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "reporter")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/reporter.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(dannypsnl))
---
+++ scribblings/reporter.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[reporter
+                    racket/base]]
+
+@title{reporter}
+@author{dannypsnl}
+
+@defmodule[reporter]
+
+Package Description Here
