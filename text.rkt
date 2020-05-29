4e603b204961e9da0d037132fef74e2a3a1c59e6
dannypsnl <dannypsnl@gmail.com>
2020-05-29 20:31:11 +0800

    provide text print fundmental and append*

--- text.rkt
+++ text.rkt
@@ -1,23 +1,35 @@
 #lang typed/racket
 
+(provide text color-text
+         text-append*
+         print-text
+         (all-from-out "color.rkt"))
+
 (require/typed terminal-color
                [display-color (String #:fg Symbol -> Void)])
 (require "color.rkt")
 
-(define-type text (U color-text String))
+(define-type text (U color-text String (Listof text)))
 (struct color-text
   ([color : color]
    [text : String])
   #:transparent)
+
+(: text-append* (text * -> text))
+(define (text-append* . ts)
+  ts)
 
 (: print-text (text -> Void))
 (define (print-text t)
   (match t
     ([color-text color str]
      (let ([c-atom (match color
-                      ([color:red] 'red)
-                      ([color:blue] 'blue)
-                      )])
+                     ([color:red] 'red)
+                     ([color:blue] 'blue)
+                     )])
        (display-color str #:fg c-atom)
        ))
-    (str (display str))))
+    ([? string?] (display t))
+    ([? list?] (for-each
+             (λ ([t : text]) (print-text t))
+             t))))
