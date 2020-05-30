e9fb0b6fb6f12c60183cf0adf673d872b8f5b061
dannypsnl <dannypsnl@gmail.com>
2020-05-30 12:31:40 +0800

    dep: update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "reporter")
-(define deps '("base"))
+(define deps '("base" "ansi-color"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/reporter.scrbl" ())))
 (define pkg-desc "Description Here")
