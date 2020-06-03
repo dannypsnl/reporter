60d25e41cb3d43b20a4091e59a81b2e3180bdca4
dannypsnl <dannypsnl@gmail.com>
2020-06-03 21:05:32 +0800

    (#10) pos from srcloc
    resolve #10

--- position.rkt
+++ position.rkt
@@ -1,8 +1,25 @@
 #lang typed/racket
 
-(provide Pos Pos-line Pos-column)
+(provide (struct-out Pos)
+         Pos-from-srcloc)
+
+(require/typed syntax/srcloc
+  [source-location-line (-> srcloc Integer)]
+  [source-location-column (-> srcloc Integer)])
 
 (struct Pos
   ([line : Integer]
    [column : Integer])
   #:transparent)
+
+(define (Pos-from-srcloc [loc : srcloc])
+  : Pos
+  (let ([line (source-location-line loc)]
+        [column (source-location-column loc)])
+    (Pos line column)))
+
+(module+ test
+  (require typed/rackunit))
+(module+ test
+  (check-equal? (Pos-from-srcloc (make-srcloc 'here 1 2 0 0))
+                (Pos 1 2)))
