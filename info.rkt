eaba353763fccc7ea2cf9d742f15c117e91b329a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-20 01:47:16 +0800

    [release] v0.1.0

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/reporter.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.0")
+(define version "0.1.0")
 (define pkg-authors '(dannypsnl))
